629f1bf2ecd837435a721edf19134ffcc5269eb3
abolfazl jamshidi <93490760+abolfazlj10@users.noreply.github.com>
2024-11-26 08:58:20 -0800

    Update instagram ID

--- app/components/thirdScreen.js
+++ app/components/thirdScreen.js
@@ -8,7 +8,7 @@ export default function ThirdScreen () {
             <div className='socialMediaLinks'>
             <div className='containerLinks'>
                 <div className='text-[#D44638] blob'><a href='mailto:abolfazljamshidi101010@gmail.com'>جیمیل</a></div>
-                <div className='blob min-[750px]:-mr-10'><a className="instagramLink" href='https://www.instagram.com/abolfazlj_10'>اینستاگرام</a></div>
+                <div className='blob min-[750px]:-mr-10'><a className="instagramLink" href='https://www.instagram.com/abolfazl_j10'>اینستاگرام</a></div>
                 <div className='blob text-[#0A66C2] min-[750px]:-mr-10 max-[750px]:-ml-40 max-[750px]:-mt-10'><a href='https://www.linkedin.com/in/abolfazljamshidi/'>لینکدین</a></div>
             </div>
             <div className='containerLinks'>
